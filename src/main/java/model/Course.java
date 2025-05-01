bed08055c079dd24412a7ab65312e41331e4463b
Dinuki85 <prarthanadinuki2@gmail.com>
2025-05-01 14:57:30 +0530

    Added course model details

--- src/main/java/model/Course.java
+++ src/main/java/model/Course.java
@@ -1,4 +1,14 @@
 package model;
 
 public class Course {
+    private String code;
+    private String name;
+    private int credits;
+
+    public Course(String name, String code, int credits) {
+        super();
+        this.name = name;
+        this.code = code;
+        this.credits = credits;
+    }
 }
