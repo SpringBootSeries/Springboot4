a66720e07c662a7048e51638b77cf7d127f87177
Dinuki85 <prarthanadinuki2@gmail.com>
2025-05-01 15:04:52 +0530

    Added some student objects in App Controller

--- src/main/java/controller/AppControllers.java
+++ src/main/java/controller/AppControllers.java
@@ -1,4 +1,17 @@
 package controller;
 
+import model.Student;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app")
+
 public class AppControllers {
+    Student s1 = new Student("2020ict65","Chandru",23,"IT",3.66);
+    Student s2 = new Student("2020bm31","Tharu",24,"BM",3.80);
+    Student s3 = new Student("2020ASB76","Suthan",23,"BIO Sceince",4.0);
+    Student s4 = new Student("2020ICt34","Chamari",25,"ICT",3.59);
+    Student s5 = new Student("2020ICT67","Chawendra",30,"ITa",3.25);
+
 }
